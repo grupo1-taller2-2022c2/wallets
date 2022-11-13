90197717a912b8e5504026c7077763feadfa6338
Agustin Gabrielli <agusgabrielli9@gmail.com>
2022-11-13 11:49:15 -0300

    Conexion a la base de mongodb con mongoose

--- src/server.js
+++ src/server.js
@@ -1,9 +1,20 @@
 const config = require("./config");
 const services = require("./services/services")({ config });
 const routes = require("./routes");
+const mongoose = require('mongoose');
+
 
 // Require the framework and instantiate it
 const fastify = require("fastify")({ logger: true });
+
+// Connect fastify to mongoose
+mongoose
+  .connect('mongodb+srv://admin:grupo1@fiuberwalletsdev.qy29k8l.mongodb.net/?retryWrites=true&w=majority', {dbName: 'fiuber'})
+  .then(() => {
+  console.log('Connected to the Database.');
+  })
+  .catch(err => console.error(err));
+
 
 // Declares routes
 routes.forEach(route => fastify.route(route({ config, services })));
